06149bab6739a5e5bb118e57c4100276d413924f
superdyoll <superdyoll@gmail.com>
2015-03-03 14:49:39 +0000

    Complex class

---
+++ src/Fractals/Complex.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fractals;
+
+/**
+ *
+ * @author Lloyd
+ */
+public class Complex {
+    
+    private float real;
+    private float imaginary;
+    
+    
+}
