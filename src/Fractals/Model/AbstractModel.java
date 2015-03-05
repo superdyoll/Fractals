25814016cf567f355405cfe38ab33baf699265d7
superdyoll <superdyoll@gmail.com>
2015-03-05 13:37:46 +0000

    added other abstract MVC parts

--- src/Fractals/Model/AbstractModel.java
+++ src/Fractals/Model/AbstractModel.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Abstract Model copied from oracle website
+ * src: http://www.oracle.com/technetwork/articles/javase/index-142890.html
  */
 package Fractals.Model;
 
---
+++ src/Fractals/View/AbstractViewPanel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fractals.View;
+
+import java.beans.PropertyChangeEvent;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Lloyd
+ */
+public abstract class AbstractViewPanel extends JPanel{
+
+    public void modelPropertyChange(PropertyChangeEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
