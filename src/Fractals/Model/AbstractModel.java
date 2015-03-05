d6ea937b93bdfc5f2c8e7a035c6251dff29cea91
superdyoll <superdyoll@gmail.com>
2015-03-05 13:25:36 +0000

    Added abstract model

---
+++ src/Fractals/Model/AbstractModel.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fractals.Model;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ * @author Lloyd
+ */
+public abstract class AbstractModel {
+    protected PropertyChangeSupport propertyChangeSupport;
+
+    public AbstractModel()
+    {
+        propertyChangeSupport = new PropertyChangeSupport(this);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
+    protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
+}
