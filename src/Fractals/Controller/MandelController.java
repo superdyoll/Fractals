1b3aafee7ef5048dce20d30ade8e66478ab78f68
superdyoll <superdyoll@gmail.com>
2015-03-09 15:02:16 +0000

    wikipedia colours :)

--- src/Fractals/Controller/MandelController.java
+++ src/Fractals/Controller/MandelController.java
@@ -8,6 +8,7 @@ package Fractals.Controller;
 import Fractals.Maths.Complex;
 import Fractals.Model.MandelModel;
 import Fractals.View.MandelView;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 /**
@@ -15,7 +16,8 @@ import java.awt.image.BufferedImage;
  * @author Lloyd
  */
 public class MandelController {
-    
+
+    private Color[] colorMap;
 
     public BufferedImage drawMandel(int width, int height, int xCenter, int yCenter, int iterations, int zoom) {
         System.out.println("Drawing mandel");
@@ -25,16 +27,81 @@ public class MandelController {
         System.out.println(height + " heigh");
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                Complex point = new Complex(((float)(x-xCenter)/zoom), ((float)(y-yCenter)/zoom));
+                Complex point = new Complex(((float) (x - xCenter) / zoom), ((float) (y - yCenter) / zoom));
                 int iter = newMandel.calculateMandelPoint(point, iterations);
-                graph.setRGB(x, y, iter | (iter << 8));
+                Color c = getColour(iterations, iter);
+                int rgb = c.getRGB();
+                graph.setRGB(x, y, rgb);
             }
         }
         System.out.println("For loops finished");
         return graph;
     }
-    
-    public static void main (String [] args){
+
+    public Color getColour(int maxIterations, int iter) {
+        if (iter < maxIterations && iter > 0) {
+            int i = iter % 16;
+            Color c;
+            switch (i) {
+                case 0:
+                    c = new Color(66, 30, 15);
+                    break;
+                case 1:
+                    c = new Color(25, 7, 26);
+                    break;
+                case 2:
+                    c = new Color(9, 1, 47);
+                    break;
+                case 3:
+                    c = new Color(4, 4, 73);
+                    break;
+                case 4:
+                    c = new Color(0, 7, 100);
+                    break;
+                case 5:
+                    c = new Color(12, 44, 138);
+                    break;
+                case 6:
+                    c = new Color(24, 82, 177);
+                    break;
+                case 7:
+                    c = new Color(57, 125, 209);
+                    break;
+                case 8:
+                    c = new Color(134, 181, 229);
+                    break;
+                case 9:
+                    c = new Color(211, 236, 248);
+                    break;
+                case 10:
+                    c = new Color(241, 233, 191);
+                    break;
+                case 11:
+                    c = new Color(248, 201, 95);
+                    break;
+                case 12:
+                    c = new Color(255, 170, 0);
+                    break;
+                case 13:
+                    c = new Color(204, 128, 0);
+                    break;
+                case 14:
+                    c = new Color(153, 87, 0);
+                    break;
+                case 15:
+                    c = new Color(106, 52, 3);
+                    break;
+                default:
+                    c = new Color(0,0,0);
+                    break;
+            }
+            return c;
+        } else {
+            return new Color (0,0,0);
+        }
+    }
+
+    public static void main(String[] args) {
         new MandelView().setVisible(true);
     }
 
